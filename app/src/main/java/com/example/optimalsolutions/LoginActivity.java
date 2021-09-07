c8ffdb066646367e227b2e148b83cf1f63e6d1f7
Snehal Sutar <snehal.v.sutar@gmail.com>
2021-09-06 22:07:19 -0400

    initial commit

---
+++ app/src/main/java/com/example/optimalsolutions/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.optimalsolutions;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
