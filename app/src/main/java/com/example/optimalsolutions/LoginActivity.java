8f8fae90dc902ba5e73cfe3015c54ec98fb99b9f
DESKTOP-S5CUQG2\visha <vishalvsutar@gmail.com>
2021-09-08 09:23:10 +0530

    Ticket Activity is created

--- app/src/main/java/com/example/optimalsolutions/LoginActivity.java
+++ app/src/main/java/com/example/optimalsolutions/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.optimalsolutions;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,5 +13,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    public void onLoginButtonClick(View view){
+        startActivity(new Intent(this, TicketsActivity.class));
+
     }
 }
---
+++ app/src/main/java/com/example/optimalsolutions/NewTicketActivity.java
@@ -0,0 +1,14 @@
+package com.example.optimalsolutions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewTicketActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_ticket);
+    }
+}
