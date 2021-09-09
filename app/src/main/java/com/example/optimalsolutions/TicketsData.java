890048f6c3746c41ffe98a3329ee57d295d73707
DESKTOP-S5CUQG2\visha <vishalvsutar@gmail.com>
2021-09-09 08:09:23 +0530

    Java Class ticket data created with fields

---
+++ app/src/main/java/com/example/optimalsolutions/TicketsData.java
@@ -0,0 +1,16 @@
+package com.example.optimalsolutions;
+
+public class TicketsData {
+
+    String title;
+    String issue;
+    String description;
+    String resolution;
+    String dateOfIssue;
+    long id;
+
+
+
+
+
+}
